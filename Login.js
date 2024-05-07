907b8dfc138d98344bd2cd5eab7acdde6a0f1be5
Seowoo Kim <gkrtks030611@gmail.com>
2024-05-07 11:39:29 +0900

    수정

---
+++ Login.js
@@ -0,0 +1,14 @@
+document.getElementById('icon').addEventListener('click',function(){
+    var SideMenu=document.getElementById('SideMenu');
+    if (SideMenu.style.display==='none') {
+        SideMenu.style.display='block';
+    } else {
+        SideMenu.style.display='none';
+    }
+})
+document.getElementById('exit').addEventListener('click',function(){
+    var SideMenu=document.getElementById('SideMenu');
+    if (SideMenu.style.display==='block') {
+        SideMenu.style.display='none';
+    }
+})
